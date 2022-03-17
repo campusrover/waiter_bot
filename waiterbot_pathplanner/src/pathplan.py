1714b4b245bce72f1b5d1328c34a9ef7f9dc5073
Harry Z <66760899+harryzhu626@users.noreply.github.com>
2022-03-17 20:25:56 +0000

    Add files via upload
    the pathplanner package which translate location string to move_base call

---
+++ waiterbot_pathplanner/src/pathplan.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import rospy, actionlib
+from std_msgs.msg import String
+from loc_dictionary import coordinate_dict
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+def goal_pose(loc_coordinate):
+    goal_pose = MoveBaseGoal()
+    goal_pose.target_pose.header.frame_id = 'map'
+    goal_pose.target_pose.pose.position.x = loc_coordinate[0]
+    goal_pose.target_pose.pose.position.y = loc_coordinate[1]
+    goal_pose.target_pose.pose.position.z = loc_coordinate[2]
+    goal_pose.target_pose.pose.orientation.x = loc_coordinate[3]
+    goal_pose.target_pose.pose.orientation.y = loc_coordinate[4]
+    goal_pose.target_pose.pose.orientation.z = loc_coordinate[5]
+    goal_pose.target_pose.pose.orientation.w = loc_coordinate[6]
+    return goal_pose
+
+def loc_cb(msg):
+    location_name = msg.data
+    location_coordinate = coordinate_dict[location_name]
+    goal = goal_pose(location_coordinate)
+    
+    print("Destination: ", msg.data, "-Coordinates: ", location_coordinate)
+    
+    client.send_goal(goal)
+    client.wait_for_result()
+    
+
+if __name__ == '__main__':
+    # init node
+    rospy.init_node("pathplanner")
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    client.wait_for_server()
+
+    rate = rospy.Rate(1)
+
+    while not rospy.is_shutdown():
+        print("Pathplanner Running")
+        # subscribe to ui topic
+        sub_location = rospy.Subscriber('location_string', String, loc_cb)
+        rate.sleep()
