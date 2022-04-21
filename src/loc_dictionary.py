c5c428ffd6c9d6f78a85df727fa5c5517e53003b
Harry Z <66760899+harryzhu626@users.noreply.github.com>
2022-04-21 21:30:07 +0000

    Add files via upload

--- src/loc_dictionary.py
+++ src/loc_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-coordinate_dict = { "pizza-table": [ -1, 1.5, 0, 0, 0, 0, 1,0], 
-                    "drink-table": [ 0, 1, 0, 0, 0, 0, 1.0],
-                    "dining": [ -1, 0.5, 0, 0, 0, 0, 1.0],
+coordinate_dict = { "pizza-table": [ 0, -1, 0, 0, 0, 0, 1,0], 
+                    "drink-table": [ 1, 1, 0, 0, 0, 0, 1.0]
                     }
