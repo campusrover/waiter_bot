4bcebfb96bd2fafe432f553e00d03c48c7ebf119
Harry Z <66760899+harryzhu626@users.noreply.github.com>
2022-04-29 11:17:13 -0400

    Update loc_dictionary.py

--- src/loc_dictionary.py
+++ src/loc_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-coordinate_dict = { "pizza-table": [ 0, -1, 0, 0, 0, 0, 1,0], 
-                    "drink-table": [ 1, 1, 0, 0, 0, 0, 1.0]
+coordinate_dict = { "pizza-table": [ 2, 0, 0, 0, 0, 0, 1.0], 
+                    "drink-table": [ -1, 0, 0, 0, 0, 0, 1.0]
                     }
